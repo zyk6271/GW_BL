f40070786811713495053214a8208e10d67ea973
少年老王 <710454786@qq.com>
2022-03-24 14:42:42 +0800

    v1.0.1
    1.重新规定了BL的分区大小

--- storage/fal_cfg.h
+++ storage/fal_cfg.h
@@ -45,8 +45,8 @@ extern struct fal_flash_dev nor_flash0;
 /* partition table */
 #define FAL_PART_TABLE                                                               \
 {                                                                                    \
-    {FAL_PART_MAGIC_WORD,  "boot", "stm32_onchip", 0*1024,128*1024, 0},           \
-    {FAL_PART_MAGIC_WORD,  "app", "stm32_onchip", 128*1024,384*1024, 0},           \
+    {FAL_PART_MAGIC_WORD,  "boot", "stm32_onchip", 0*1024,64*1024, 0},           \
+    {FAL_PART_MAGIC_WORD,  "app", "stm32_onchip", 64*1024,448*1024, 0},           \
     {FAL_PART_MAGIC_WORD,  "easyflash", NOR_FLASH_DEV_NAME,  0, 1024*1024, 0}, \
     {FAL_PART_MAGIC_WORD,  "factory", NOR_FLASH_DEV_NAME, 1024*1024, 512*1024, 0}, \
     {FAL_PART_MAGIC_WORD,  "download", NOR_FLASH_DEV_NAME, 1536*1024, 512*1024, 0}, \
